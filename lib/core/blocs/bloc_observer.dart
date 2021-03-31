a1fdb904d20ae83bd7eecdfd06129969084ce282
theiskaa <59066341+theiskaa@users.noreply.github.com>
2021-03-31 16:11:59 +0400

    added logger into bloc_observer.dart

--- lib/core/blocs/bloc_observer.dart
+++ lib/core/blocs/bloc_observer.dart
@@ -1,21 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:devexam/core/system/log.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print('[ onEvent $event ]');
+    Log.d(" onEvent $event ");
     super.onEvent(bloc, event);
   }
 
   @override
   onTransition(Bloc bloc, Transition transition) {
-    print('[ onTransition $transition ]');
+    Log.d("[ onTransition $transition ]");
     super.onTransition(bloc, transition);
   }
 
   @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print(' === [ onError $error ] ===');
+    Log.e(cubit, error, stackTrace);
     super.onError(cubit, error, stackTrace);
   }
 }
